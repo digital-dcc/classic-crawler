048a733a5b6fa48e1319a8f130bb1d43d3688b90
Richard Walker <digitalsadhu@gmail.com>
2024-09-22 15:00:51 +1200

    try to inject env vars for auth

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -19,7 +19,20 @@ const initApp = () => {
   if (import.meta.env.PROD) {
     console.info("PROD env detected. Using default service account.");
     // Use default config in firebase functions. Should be already injected in the server by Firebase.
-    return initializeApp();
+    return initializeApp({
+      credential: cert({
+        type: "service_account",
+        project_id: process.env.FIREBASE_PROJECT_ID,
+        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+        private_key: process.env.FIREBASE_PRIVATE_KEY,
+        client_email: process.env.FIREBASE_CLIENT_EMAIL,
+        client_id: process.env.FIREBASE_CLIENT_ID,
+        auth_uri: process.env.FIREBASE_AUTH_URI,
+        token_uri: process.env.FIREBASE_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_CERT_URL,
+        client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+      } as ServiceAccount),
+    });
   }
   console.info("Loading service account from env.", serviceAccount);
   return initializeApp({
